4631e4772df6a94bfce175f2f55315183ebb44ca
Isaac Hillaker <hillakerisaac@gmail.com>
2021-03-03 13:44:56 -0600

    Added JS file to toggle mobile menu. Basic function, style, and animation complete. Needs tweaking later.

---
+++ mobileMenu.js
@@ -0,0 +1,8 @@
+function onClickMenu() {
+  // Setup 'change' class for 'menu' (Mobile Menu)
+  document.getElementById("menu").classList.toggle("change");
+  // Setup 'change' class for 'mainNav' (Mobile Menu)
+  document.getElementById("mainNav").classList.toggle("change");
+  // Setup 'changeBackground' class for 'menuBackground' (Mobile Menu)
+  document.getElementById("menuBackground").classList.toggle("changeBackground");
+}
