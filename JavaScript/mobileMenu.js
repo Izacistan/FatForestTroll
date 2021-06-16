a69047728e7330447acdc35ebddafad59f66ab5b
Isaac Hillaker <hillakerisaac@gmail.com>
2021-06-16 14:51:34 -0500

    Added new SVG image. Cleaning out JS code.

--- JavaScript/mobileMenu.js
+++ JavaScript/mobileMenu.js
@@ -1,8 +1,1 @@
-function onClickMenu() {
-  // Setup 'change' class for 'menu' (Mobile Menu)
-  document.getElementById("menu").classList.toggle("change");
-  // Setup 'change' class for 'mainNav' (Mobile Menu)
-  document.getElementById("mainNav").classList.toggle("change");
-  // Setup 'changeBackground' class for 'menuBackground' (Mobile Menu)
-  document.getElementById("menuBackground").classList.toggle("changeBackground");
-}
+
